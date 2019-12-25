18d89beb51bee239dfc8d8588d563a37a32837ad
Killa4 <killagib@gmail.com>
2019-12-25 06:01:16 -0500

    New Update to allow multiple bots in single instance!

--- index.js
+++ index.js
@@ -1,100 +1,96 @@
-const WebRcon = require('webrconjs') 
+const WebRcon = require('webrconjs')
 const Discord = require('discord.js')
-const bot = new Discord.Client({autoReconnect:true});
-const config = require('config.json')('./config.json')
-let connect = null;
+const bot = new Discord.Client();
+const config = require('./config.json')
 
-// Login to discord 
-bot.on('ready', () => {
-	console.log('Logged in as', bot.user.tag)
-}) 
-//Catch discord disconnects
-bot.on('disconnected', () => {
-    console.log('Discord bot Error / Got disconnected (Trying to reconnect)')
-});
+config.Servers.forEach((server, index) => {
+    server.name = `${server.IP}:${server.Port}/${index}`
+    server.rcon = new WebRcon(server.IP, server.Port)
+    server.connected = false;
+    server.bot = new Discord.Client();
+    server.bot.login(server.DiscordToken)
 
-// New rcon client 
-let rcon = new WebRcon(config.IP, config.Port)
- 
-// Connect to rcon function / Get serverinfo data
-rcon.on('connect', function() {
-    try {
-    connect = true;
-    console.log('CONNECTED');
-    lastMessage = '';
-    bot.user.setActivity('Server Connecting...');
-    } catch {
-    }
-    if (connect === true) {
-        function getData() {
+    let waitingForMessage = false
+    let lastMessage = ''
+
+    // Login to discord 
+    server.bot.on('ready', () => {
+        console.log('Logged in as', server.bot.user.tag)
+        server.bot.user.setActivity('Server Connecting...');
+        reconnect()
+    })
+
+    server.rcon.on('connect', function () {
+        try {
+            server.connected = true;
+            console.log(server.name, 'CONNECTED');
+            lastMessage = '';
+            server.bot.user.setActivity('Server Connecting...');
+        } catch {
+        }
+        if (server.connected === true) {
+            function getData() {
+                try {
+                    server.rcon.run('serverinfo', 0);
+                } catch {
+                }
+            }
+            getData();
             try {
-            rcon.run ('serverinfo', 0);
+                // Rerun command over set interval 
+                setInterval(getData, Math.max(5, config.SetInterval || 5) * 1000);
             } catch {
             }
         }
-        getData();
+    });
+
+    server.rcon.on('message', function (msg) {
+        // Parse Messages
+        const data = JSON.parse(msg.message)
+        // Set Discord status (No idea why it returns undefined sometimes simple fix added to prevent it.)
+        if (data.Players === undefined) {
+            return;
+        } else if (data.Queued > 0) {
+            setMessage(`(${data.Players}/${data.MaxPlayers} (${data.Queued}) Queued!)`);
+            waitForMessage = true
+        } else if (data.Joining === 0) {
+            setMessage(`(${data.Players}/${data.MaxPlayers} Online!)`);
+            waitForMessage = true
+        } else {
+            setMessage(`(${data.Players}/${data.MaxPlayers} (${data.Joining}) Joining!)`);
+            waitForMessage = true
+        }
+    })
+    //Spam prevention to discord api (If message is the same it will not paste over and over!)
+    function setMessage(newMessage) {
+        if (waitingForMessage === true || newMessage === lastMessage) {
+            return
+        } else {
+            server.bot.user.setActivity(newMessage);
+            console.log(server.name, newMessage);
+            lastMessage = newMessage
+        }
+    }
+    // Disconnect function to know when the rcon gets disconnected / server restarts
+    server.rcon.on('disconnect', function () {
+        server.connected = false;
+        server.bot.user.setActivity('Server Offline...');
+        console.log(server.name, "Server Offline");
+        // Reconnect if server goes offline 
+        if (server.connected === false) {
+            try {
+                console.log(server.name, "TRYING TO RECONNECT");
+                setInterval(reconnect, Math.max(5, 5 || 5) * 1000);
+            } catch {
+            }
+        }
+    })
+
+    // Connect / Reconnect function
+    function reconnect() {
         try {
-        // Rerun command over set interval 
-        setInterval(getData, Math.max(5, config.SetInterval || 5) * 1000);
+            server.rcon.connect(server.RconPassword)
         } catch {
         }
     }
 });
-
-rcon.on('message', function(msg) {
-    // Parse Messages
-    const data = JSON.parse(msg.message)  
-    // Set Discord status (No idea why it returns undefined sometimes simple fix added to prevent it.)
-    if (data.Players === undefined){
-        return;
-    } else if (data.Queued > 0){
-        setMessage(`(${data.Players}/${data.MaxPlayers} (${data.Queued}) Queued!)`);
-        waitForMessage = true
-    } else if (data.Joining === 0){
-        setMessage(`(${data.Players}/${data.MaxPlayers} Online!)`);
-        waitForMessage = true
-    } else {
-        setMessage(`(${data.Players}/${data.MaxPlayers} (${data.Joining}) Joining!)`);
-        waitForMessage = true
-    }
-})
-//Spam prevention to discord api (If message is the same it will not paste over and over!)
-let waitingForMessage = false
-let lastMessage = ''
-let newMessage = ''
-function setMessage(newMessage) {
-    if (waitingForMessage === true || newMessage === lastMessage) {
-        return
-    } else {
-        bot.user.setActivity(newMessage);
-        console.log(newMessage);
-        lastMessage = newMessage
-    }
-}
-// Disconnect function to know when the rcon gets disconnected / server restarts
-rcon.on('disconnect', function() {
-    connect = false;
-    bot.user.setActivity('Server Offline...');
-    console.log("Server Offline");
-// Reconnect if server goes offline 
-    if (connect === false){
-        try {
-        console.log("TRYING TO RECONNECT");
-        setInterval(reconnect, Math.max(5, 5 || 5) * 1000);
-        } catch {
-        }
-    }
-})
-
-// Connect / Reconnect function
-function reconnect() {
-    try {
-        rcon.connect(config.RconPassword)
-    } catch {
-    }
-}
-
-//First connection 
-reconnect()
-// Login to discord bot
-bot.login(config.DiscordToken)
